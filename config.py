783873afb094d508d3331778fc5989782fc7df5e
Sodoghiam <104337681+Sodoghiam@users.noreply.github.com>
2023-06-02 16:05:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,10 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001725487477"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "سلام.\
+\
+اول باید کانال بکاپ جوین شی تا بتونی فیلم بگیری  .\
+کانال بکاپ : \n https://t.me/joinchat/nVWYqlrYP8w3NTAx\n-(اول باید کانال بالا عضو شید بعد دوباره فیلمی که میخواید بگیرید رو انتخاب کنید )-")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +49,10 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "سلام.\
+\
+اول باید کانال بکاپ جوین شی تا بتونی فیلم بگیری  .\
+کانال بکاپ : \n https://t.me/joinchat/nVWYqlrYP8w3NTAx\n-(اول باید کانال بالا عضو شید بعد دوباره فیلمی که میخواید بگیرید رو انتخاب کنید )-")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
