e9b4c0f0a6bf3bf79634e0e1cd99b6921293d738
Sodoghiam <104337681+Sodoghiam@users.noreply.github.com>
2023-11-11 11:28:44 +0330

    farsi start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "چتل بکاپ",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'عضو شدم',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
