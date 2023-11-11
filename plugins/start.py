7c6b700b095f6414cfc76fba8589d6387e7b8ca9
Sodoghiam <104337681+Sodoghiam@users.noreply.github.com>
2023-11-11 11:33:16 +0330

    farsi 2 start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "چتل بکاپ",
+                "کانال بکاپ",
                 url = client.invitelink)
         ]
     ]
